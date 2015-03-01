c41456f7adbbc329549467733ee027bffb668f62
Christian Barra <barrachri@gmail.com>
2015-03-01 01:43:57 +0100

    db directory changed

--- ephelpdesk/settings.py
+++ ephelpdesk/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'ephelpdesk.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data/helpdesk.db'),
     }
 }
 
